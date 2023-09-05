0fcc6d4403b739453b13a9255baefe9760c9a1f4
Ammar Ehab <ammaruchia29@gmail.com>
2023-09-05 14:45:55 +0300

    init file

---
+++ main.py
@@ -0,0 +1,9 @@
+adj  = input("Adjective: ")
+verb1  = input("First Verb: ")
+verb2  = input("Second Verb: ")
+famouse_person  = input("Famous Person: ")
+
+madlib = f"Computer programming is so {adj }! It makes me so excited all the time because \
+    I love to {verb1}. stay hydrated and {verb2} like you are {famouse_person}!"
+
+print(madlib)
